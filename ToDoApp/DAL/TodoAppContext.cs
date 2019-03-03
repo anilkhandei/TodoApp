8ab3f0b7219e396fef656c0ddd5771069b2ddbb8
anil khandei <anil.khandei@hotmail.com>
2019-03-02 16:09:07 -0800

    commit new entity framework To do App

---
+++ ToDoApp/DAL/TodoAppContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.DAL
+{
+    public class TodoAppContext:DbContext
+    {
+        public TodoAppContext(DbContextOptions<TodoAppContext> options):base(options){}
+        public DbSet<TodoItem> todoItems { get; set; }
+    }
+}
